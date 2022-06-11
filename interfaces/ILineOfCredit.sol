293299175e3be60eeb00cbaad1ad649b776c1353
Vedran Bidin <vedran@bidin.me>
2022-06-11 23:42:22 +0300

    Streamline LineOfCredit interface

--- interfaces/ILineOfCredit.sol
+++ interfaces/ILineOfCredit.sol
@@ -1,11 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @title
-/// @notice
-/// @dev
 interface ILineOfCredit {
-    // TODO: Add errors and events, as well as structs? (if needed).
+    /***************/
+    /*** Structs ***/
+    /***************/
+
+    /**************/
+    /*** Events ***/
+    /**************/
+
+    /**************/
+    /*** Errors ***/
+    /**************/
+
+    /********************************/
+    /*** State-Changing Functions ***/
+    /********************************/
+
+    // Adjustment of available credit is done through the use of the `approve()` function.
+
+    /**********************/
+    /*** View Functions ***/
+    /**********************/
 
     /// @notice Returns the address of the contract that represents the underlying asset.
     /// @dev    The contract must be ERC-20 compatible.
@@ -40,30 +57,4 @@ interface ILineOfCredit {
     /// @dev    Includes the oustanding principal plus all accumulated interest.
     /// @return debt Amount of assets owed by the debtor.
     function debt() external view returns (uint debt);
-
-    /// @notice Returns the maximum debt after which a default can be triggered.
-    /// @dev    This value can be a constant threshold or can be calculated dynamically.
-    ///         Returns `type(uint).max` if the debt ceiling does not exist.
-    /// @return debtCeiling The debt ceiling.
-    function debtCeiling() external view returns (uint debtCeiling);
-
-    /// @notice Increases or decreases the available credit.
-    /// @dev    In order to increase the credit assets must be transferred into the contract first (or approved).
-    ///         Decreasing credit causes assets to be transferred out of the contract.
-    ///         Can only be called by the creditor.
-    /// @param  adjustment Amount of assets to add or remove.
-    /// @return credit     Credit available after the adjustment.
-    function adjustCredit(int adjustment) external returns (uint credit);
-
-    // TODO: Aggregate all assets into a separate contract, setup permissioning and allowances on a per debtor basis.
-    // TODO: Functions for increasing and decreasing available credit, used by the creditor.
-    // TODO: Function for adjusting the debt ceiling (through what kind of mechanisms?).
-    // TODO: Functions for borrowing and repaying, used by the debtor.
-    // TODO: Function for closing the line of credit. Both creditor and debtor can close after all debt is repaid.
-    //       creditor can also close if the line of credit is inactive or the debt is above the default threshold.
-    // TODO: Function that allows the debtor to adjust the interest rate (gradually over time, never retroactively).
-    // TODO: Functions that allow the debtor and creditor addresses to be updated via a handshake.
-    // TODO: Functions that allow for collateral to be posted (initially and on demand) in order to increase the debt ceiling.
-    // TODO: Function for retrieving unaccounted assets accidentally sent to the contract (send to lost and found, or treasury?).
-    // TODO: Keep track of statistics of each creditor / debtor (better use The Graph for this).
 }
