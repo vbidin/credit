860c62c089c88fc74112370c13306fc86e4ac1ae
Vedran Bidin <vedran@bidin.me>
2022-06-19 22:57:47 +0300

    Remove option of using custom profiles

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e
 
-while getopts p: flag
-do
-    case "${flag}" in
-        p) profile=${OPTARG};;
-    esac
-done
-
 export FOUNDRY_CONFIG=./configs/foundry.toml
-export FOUNDRY_PROFILE=$profile
 
 forge build -o output
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,13 +4,11 @@ set -e
 while getopts p:t: flag
 do
     case "${flag}" in
-        p) profile=${OPTARG};;
         t) test=${OPTARG};;
     esac
 done
 
 export FOUNDRY_CONFIG=./configs/foundry.toml
-export FOUNDRY_PROFILE=$profile
 
 if [ -z "$test" ];
 then
