2cd07a7a9487fc84a2e0e932c47f080afc4142a8
Vedran Bidin <vedran@bidin.me>
2022-06-19 22:52:21 +0300

    Add gas reporting script

--- scripts/gas.sh
+++ scripts/gas.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
 
-# TODO: Generate gas report.
+while getopts p: flag
+do
+    case "${flag}" in
+        p) profile=${OPTARG};;
+    esac
+done
+
+export FOUNDRY_CONFIG=./configs/foundry.toml
+export FOUNDRY_PROFILE=$profile
+
+forge test -o output --gas-report
