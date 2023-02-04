36c28acd3d94e8fd43ef8460bf34f6494b450eeb
Vedran Bidin <vedran@bidin.me>
2023-02-04 22:15:13 +0200

    Update configuration files

--- tests/SampleContract.t.sol
+++ tests/SampleContract.t.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// TODO: Add submodules for logging and assertions.
-
 contract SampleContractTests {
     function setUp() external virtual {}
 
-    function test_success() external {}
+    function test() external {}
 }
